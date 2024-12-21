dba781a0fc584d4b1b0eaf0ad875b3c047441f3e
Apoorv <apoorv127@gmail.com>
2024-12-21 13:22:34 +0530

    21stDec

---
+++ src/test/java/org/selenium/aj34/screens/first.java
@@ -0,0 +1,21 @@
+package org.selenium.aj34.screens;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class first {
+    @Test
+    public void test(){
+
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://testautomationpractice.blogspot.com/");
+        System.out.println(driver.getTitle());
+        System.out.println("Hello World");
+        System.out.print("Apoorv");
+        System.out.println("HELLO");
+        System.out.println("James");
+    }
+}
